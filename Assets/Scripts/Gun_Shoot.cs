1b87e69edc379bf1ccf0290d6ddb17fd96435905
Silver Burnie <AuAgments@gmail.com>
2024-09-10 13:01:43 -0300

    First iteration of silly ammo

--- Assets/Scripts/Gun_Shoot.cs
+++ Assets/Scripts/Gun_Shoot.cs
@@ -1,4 +1,4 @@
-using TMPro;
+using System.Collections.Generic;
 using UnityEngine;
 public class Gun_Shoot : MonoBehaviour
 {
@@ -6,27 +6,58 @@ public class Gun_Shoot : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
 
-    public int ammoCount = 10;
-    public float bulletForce = 10;
+    public int MaxAmmo;
+    public int ammoCount;
+    public float bulletForce;
 
+    List<int> normalAmmo = new List<int> { 1, 3, 5, 7, 13, 19, 23, 29, 39, 43, 47, 51, 55, 60, 63, 69, 70, 75, 77, 87, 90, 93, 98, 99, 100 };
+   
     private void Awake()
     {
         playerTransform = transform;
+        MaxAmmo = ammoCount;
     }
 
     private void Update()
     {
+
         if (ammoCount > 0 && Input.GetKeyDown(KeyCode.E))
         {
-            Fire();
+            if (!normalAmmo.Contains(ammoCount))
+            {
+                SillyAmmoCheck();
+            }
+            else if (normalAmmo.Contains(ammoCount))
+            {
+                Fire();
+            }
+            else
+            {
+                Debug.Log("Out of ammo");
+            }
+        }
+    }
+    private void SillyAmmoCheck()
+    {
+        if (!normalAmmo.Contains(ammoCount))
+        {
+            Debug.Log("Silly Ammo Shot");
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.AddForce(firePoint.forward * bulletForce/4, ForceMode.Impulse);
+            ammoCount--;
         }
     }
 
     public void Fire()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
-        ammoCount--;
+        if (normalAmmo.Contains(ammoCount))
+        {
+            Debug.Log("normal ammo shot");
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+            ammoCount--;
+        }
     }
 }
