a0a3fe643aef4242b05851fc17204a7e944f0c76
Silver Burnie <AuAgments@gmail.com>
2024-09-11 16:15:25 -0300

    Gun near finished
    Gun now fires 8 different bullet types, the last being reload with a cooldown. All bullets have appropriate weight and damage. Gun now uses a luck modifier, fire rate, overheated and overheated status

--- Assets/Scripts/Gun_Shoot.cs
+++ Assets/Scripts/Gun_Shoot.cs
@@ -1,25 +1,43 @@
 using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
 
 public class Gun_Shoot : MonoBehaviour
 {
     public Transform playerTransform;
     public Transform firePoint;
-    public int MaxAmmo;
+    public int maxAmmo;
     public int ammoCount;
-    public float bulletForce;
+    public float fireRate;
+    public int luckModifier;
+    public bool gunIsOverheated;
 
+    private float force;
+    private bool canFireAgain;
     // Public references for different ammo types
-    public GameObject ammoType1;
-    public GameObject ammoType2;
-    public GameObject ammoType3;
-    public GameObject ammoType4;
-    public GameObject ammoType5;
-    public GameObject ammoType6;
-    public GameObject ammoType7;
+    public GameObject bulletAmmo;
+    public GameObject pillowAmmo;
+    public GameObject chickenAmmo;
+    public GameObject lampAmmo;
+    public GameObject skullAmmo;
+    public GameObject deerAmmo;
+    public GameObject busAmmo;
+    public GameObject Overheated;
 
-    private void Awake()
+    public int bulletForce;
+    public int pillowForce;
+    public int chickenForce;
+    public int lampForce;
+    public int skullForce;
+    public int deerForce;
+    public int busForce;
+
+
+    private void Start()
     {
-        ammoCount = MaxAmmo;
+        gunIsOverheated = false;
+        canFireAgain = true;
+        ammoCount = maxAmmo;
         playerTransform = transform;
     }
 
@@ -31,47 +49,91 @@ public class Gun_Shoot : MonoBehaviour
         }
     }
 
+    private IEnumerator FireRateCoroutine()
+    {
+        yield return new WaitForSeconds(fireRate);
+        canFireAgain = true;
+    }
+
     public void Fire()
     {
-        // Generate a random number between 1 and 100
-        int randomNumber = Random.Range(1, 101);
+        if (gunIsOverheated || !canFireAgain) { return; }
+        StartCoroutine(FireRateCoroutine());
 
-        // Determine which ammo to use based on the random number
+        int randomNumber = Random.Range(1, 101) + luckModifier;
+
+        Debug.Log(randomNumber);
+
         GameObject ammoToFire = null;
 
         switch (randomNumber)
         {
-            case int n when (n >= 1 && n <= 30):
-                ammoToFire = ammoType1;
+            case int i when (i >= 1 && i <= 30):
+                ammoToFire = bulletAmmo;
+                force = bulletForce;
                 break;
-            case int n when (n >= 31 && n <= 50):
-                ammoToFire = ammoType2;
+            case int i when (i >= 31 && i <= 50):
+                ammoToFire =pillowAmmo;
+                force = pillowForce;
                 break;
-            case int n when (n >= 51 && n <= 60):
-                ammoToFire = ammoType3;
+            case int i when (i >= 51 && i <= 60):
+                ammoToFire = chickenAmmo;
+                force = chickenForce;
                 break;
-            case int n when (n >= 61 && n <= 70):
-                ammoToFire = ammoType4;
+            case int i when (i >= 61 && i <= 70):
+                ammoToFire = lampAmmo;
+                force = lampForce;
                 break;
-            case int n when (n >= 71 && n <= 80):
-                ammoToFire = ammoType5;
+            case int i when (i >= 71 && i <= 80):
+                ammoToFire = skullAmmo;
+                force = skullForce;
                 break;
-            case int n when (n >= 81 && n <= 90):
-                ammoToFire = ammoType6;
+            case int i when (i >= 81 && i <= 90):
+                ammoToFire = deerAmmo;
+                force = deerForce;
                 break;
-            case int n when (n >= 91 && n <= 100):
-                ammoToFire = ammoType7;
+            case int i when (i >= 91 && i <= 100):
+                ammoToFire = busAmmo;
+                force = busForce;
+                break;
+            case int i when (i > 100):
+                ammoToFire = Overheated;
+                gunIsOverheated = true;
                 break;
         }
 
-        if (ammoToFire != null)
+        if (ammoToFire != null && !gunIsOverheated)
         {
-            ShootBullet(ammoToFire, bulletForce);
+            ShootBullet(ammoToFire, force);
+            Debug.Log(ammoToFire);
+        }
+        else if (ammoToFire == Overheated)
+        {
+            Overheat(ammoToFire);
+            gunIsOverheated = true;
+            StartCoroutine(OverheatCoroutine());
+            
+            IEnumerator OverheatCoroutine()
+            {
+                Debug.Log("Started Coroutine at timestamp : " + Time.time);
+                yield return new WaitForSeconds(5);
+                Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+                luckModifier++;
+                gunIsOverheated = false;
+            }
         }
         else
         {
             Debug.Log("No ammo selected for the random number.");
+            Debug.Log(randomNumber + " is the random number");
         }
+    }
+
+    private void Overheat(GameObject ammoPrefab)
+    {
+        GameObject bullet = Instantiate(ammoPrefab, firePoint.position, firePoint.rotation);
+        bullet.transform.parent = firePoint.transform;
+        ammoCount--;
     }
 
     private void ShootBullet(GameObject ammoPrefab, float force)
@@ -80,5 +142,6 @@ public class Gun_Shoot : MonoBehaviour
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(firePoint.forward * force, ForceMode.Impulse);
         ammoCount--;
+        canFireAgain = false;
     }
 }
